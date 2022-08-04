653607d12ded1092d46176302e43ef995e81015a
Germán Vanni <germanvanni1@gmail.com>
2022-08-03 21:46:44 -0300

    Chapter 6; N-C

--- src/main/java/me/germanvanni/nox/Nox.java
+++ src/main/java/me/germanvanni/nox/Nox.java
@@ -45,13 +45,27 @@ public class Nox {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
 
-        for (Token token : tokens){
-            System.out.println(token);
-        }
+        Parser parser = new Parser(tokens);
+        Expr expression = parser.parse();
+
+        if (hadError) return;
+
+//        for (Token token : tokens){
+//            System.out.println(token);
+//        }
     }
 
     static void error(int line, String message){
         report(line, "", message);
+    }
+
+    static void error(Token token, String message){
+        if(token.type == TokenType.EOF){
+            report(token.line, " at end", message );
+        }
+        else{
+            report(token.line, " at '" + token.lexeme + "'", message);
+        }
     }
 
     private static void report(int line, String where, String message){
